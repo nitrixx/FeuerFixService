383e340c401877667fb4679d4870f9394cf1937e
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-10 22:55:50 +0200

    add example route to test reported model

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-// import { users } from '../../models'
+// import { users, reported } from '../../models'
 
 const routes = Router();
 
@@ -7,6 +7,25 @@ const routes = Router();
 // routes.get('/', async (req, res) => {
 //   const usrs = await users.findAll({});
 //   res.json({ users: usrs });
+// });
+
+// This is for dev only!
+// returns the number of reported answered for a user
+// routes.get('/:uId', async (req, res, next) => {
+//   const { params: { uId: UserID } } = req;
+
+//   try {
+//     const reportedAnsweres = await reported.findAll({ where: { UserID } });
+//     if (reportedAnsweres) {
+//       res.json({ reportedAnsweres: reportedAnsweres.length });
+//     } else {
+//       let err = new Error('Probably no user with this id found..');
+//       err.status = 404;
+//       return next(err);
+//     }
+//   } catch(e) {
+//     return next(e);
+//   }
 // });
 
 export default routes;
