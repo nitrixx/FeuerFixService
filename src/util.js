56006a897e3a0eea1d1deb10bb4eee9658d10563
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-20 17:13:10 +0200

    add method to check enabled flag

--- src/util.js
+++ src/util.js
@@ -3,3 +3,15 @@ export function createError(message, status) {
   err.status = status || 500;
   return err;
 }
+
+export function checkEnabledFlag(req, res, next) {
+  // This is one of the unprotected routes, skip the check
+  if (!req.user) { return next(); }
+
+  const { isEnabled } = req.user;
+  if (!isEnabled) {
+    const err = createError('Your account has to be activated by an administrator first.', 401);
+    return next(err);
+  }
+  return next();
+}
