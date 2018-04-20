8e6174039d3bbeb63b347eb4d88ee0be4e3adc08
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-20 19:28:42 +0200

    add forbidden error

--- src/commonErrors.js
+++ src/commonErrors.js
@@ -3,5 +3,6 @@ import { createError } from './util';
 const passwordsDoNotmatch = createError('Passwords do not match.', 400);
 const userNotFound = createError('User not found', 404);
 const accountNotEnabled = createError('Your account has to be activated by an administrator first.', 401);
+const forbidden = createError('You are not allowed to call this endpoint', 401);
 
-export { passwordsDoNotmatch, userNotFound, accountNotEnabled };
+export { passwordsDoNotmatch, userNotFound, accountNotEnabled, forbidden };
