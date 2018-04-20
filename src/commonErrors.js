277697efdb383771c612d514d938175a01c862d9
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-20 19:35:33 +0200

    add adminOrOwnerOnly error

--- src/commonErrors.js
+++ src/commonErrors.js
@@ -4,5 +4,12 @@ const passwordsDoNotmatch = createError('Passwords do not match.', 400);
 const userNotFound = createError('User not found', 404);
 const accountNotEnabled = createError('Your account has to be activated by an administrator first.', 401);
 const forbidden = createError('You are not allowed to call this endpoint', 401);
+const adminOrOwnerOnly = createError('Only the admin or the owner of this resource can access it.', 401);
 
-export { passwordsDoNotmatch, userNotFound, accountNotEnabled, forbidden };
+export {
+  passwordsDoNotmatch,
+  userNotFound,
+  accountNotEnabled,
+  forbidden,
+  adminOrOwnerOnly,
+};
