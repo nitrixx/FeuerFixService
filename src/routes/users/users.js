299c8a94d1734b087e4ad3ce842dd17c163b4ab8
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-20 17:04:05 +0200

    add method stub for update route

--- src/routes/users/users.js
+++ src/routes/users/users.js
@@ -1,11 +1,23 @@
 import { Router } from 'express';
 import { User } from '../../models';
+import { validate } from 'express-jsonschema';
+import { userUpdate as userUpdateSchema } from '../../schema';
 
 const routes = Router();
 
 routes.get('/', async (req, res) => {
   const users = await User.findAll({});
   res.json({ users });
+});
+
+routes.put('/', validate({ body: userUpdateSchema }) ,async (req, res, next) => {
+  const { id: userId, isAdmin, isEnabled } = req.user;
+  const {
+    username ='',
+    name = '',
+    newPassword = '',
+    confirmPassword = '',
+  } = req.body;
 });
 
 export default routes;
