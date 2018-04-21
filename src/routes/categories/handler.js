11542795d3d6fd3e67d9f9d6d6880aaffd57bdc0
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-21 23:02:07 +0200

    implement categories route

---
+++ src/routes/categories/handler.js
@@ -0,0 +1,32 @@
+import { Category, Question } from "../../models";
+import { createError } from "../../util";
+import { categoryNotFound } from "../../commonErrors";
+
+export async function getCategories() {
+  return await Category.findAll({
+    attributes: ['id', 'name']
+  });
+}
+
+export async function createCategory(name) {
+  const category = await Category.findOne({ where: { name } });
+  if (category) {
+    throw createError(`Category with name ${name} already exists`, 400);
+  }
+  return await Category.create({ name });
+}
+
+export async function prefetchCategory(categoryId) {
+  const category = await Category.findById(categoryId, {
+    attributes: ['id', 'name'],
+    include: [Question],
+  });
+  if (!category) { throw categoryNotFound; }
+  return category;
+}
+
+export async function deleteCategory(dbCategory) {
+  await dbCategory.Questions.map(async q => await q.destroy());
+  await dbCategory.destroy();
+  return { message: 'Success' };
+}
