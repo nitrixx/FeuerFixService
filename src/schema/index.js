e60c6933c3ad9d29d686c46332d35390dc930918
Dennis Kaiser <denniskaiser1993@gmail.com>
2018-04-21 23:00:15 +0200

    add category schema to exports

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,5 +1,11 @@
 import user from './user';
 import userUpdate from './userUpdate';
 import login from './login';
+import category from './category';
 
-export { user, userUpdate, login };
+export {
+  user,
+  userUpdate,
+  login,
+  category,
+};
